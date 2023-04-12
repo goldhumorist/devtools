b643634ccbe1e1adb734dab542c59cf6e011faf5
Buriakovskyi <o.buriakovskyi@sigmaukraine.com>
2023-04-12 18:30:19 +0200

    test(CICD): Add commented code for testing sonarcloud

--- src/routes/basic-routes.ts
+++ src/routes/basic-routes.ts
@@ -12,4 +12,15 @@ const test = (req: Request, res: Response) => {
   res.status(200).json({ status: 200, message: 'Test Route' });
 };
 
+// CODE SMELLS
+// const smells = () => {
+//   const number1 = 1;
+//   const number2 = 1;
+
+//   console.log('Some log');
+//   console.log('Some log1');
+//   console.log('Some log2');
+//   console.log('result', { number1, number2 });
+// };
+
 export { ping, greeting, test };
