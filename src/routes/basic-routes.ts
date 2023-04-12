90a0ec6723a90812d8ebf42263677e6f40831c1a
Buriakovskyi <o.buriakovskyi@sigmaukraine.com>
2023-04-12 14:58:17 +0200

    feat(CICD): Add new test route

--- src/routes/basic-routes.ts
+++ src/routes/basic-routes.ts
@@ -8,4 +8,8 @@ const greeting = (req: Request, res: Response) => {
   res.status(200).json({ status: 200, message: 'Hello World!' });
 };
 
-export { ping, greeting };
+const test = (req: Request, res: Response) => {
+  res.status(200).json({ status: 200, message: 'Test Route' });
+};
+
+export { ping, greeting, test };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import { greeting, ping } from './basic-routes';
+import { greeting, ping, test } from './basic-routes';
 
 const router = express.Router();
 
 const basicRoutes = () => {
   router.get('/', greeting);
   router.get('/ping', ping);
+  router.get('/test', test);
 
   return router;
 };
