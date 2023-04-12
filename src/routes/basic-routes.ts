6fe31dfcf41362c714cb93eade1760dd06970f99
Buriakovskyi <o.buriakovskyi@sigmaukraine.com>
2023-04-12 09:04:36 +0200

    test(CI/CD): Add error in code to test ci/cd

--- src/routes/basic-routes.ts
+++ src/routes/basic-routes.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 
 const ping = (req: Request, res: Response) => {
+  // Code for testing puprose -> trigger ci/cd fails
+  res.someFunction();
   res.status(200).json({ status: 200, message: 'pong' });
 };
 
